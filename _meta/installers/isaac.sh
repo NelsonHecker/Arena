9dd869758c6adc737352b8811692a701ec33527c
Vova Shcherbyna <dev@voshch.dev>
2025-11-07 23:10:48 +0100

    slimmer isaac install

--- _meta/installers/isaac.sh
+++ _meta/installers/isaac.sh
@@ -35,13 +35,14 @@ install(){
     #Install typegaurd dependencies
     python -m pip install typeguard
     
-    #Install isaac sim
-    echo "Downloading Isaac sim ..."
-    python -m pip install isaacsim==4.2.0.2 --extra-index-url https://pypi.nvidia.com
-    #Install Isaac sim - python package
-    echo "Downloading Isaac sim - python package ..."
-    python -m pip install isaacsim-extscache-physics==4.2.0.2 isaacsim-extscache-kit==4.2.0.2 isaacsim-extscache-kit-sdk==4.2.0.2 --extra-index-url https://pypi.nvidia.com
-    python -m pip install isaacsim-kernel isaacsim-app isaacsim-asset isaacsim-benchmark isaacsim-code-editor isaacsim-core isaacsim-cortex isaacsim-example isaacsim-gui isaacsim-replicator isaacsim-rl isaacsim-robot isaacsim-robot-motion isaacsim-robot-setup isaacsim-ros1 isaacsim-ros2 isaacsim-sensor isaacsim-storage isaacsim-template isaacsim-test isaacsim-utils --extra-index-url https://pypi.nvidia.com
+    # zip install for now
+    # #Install isaac sim
+    # echo "Downloading Isaac sim ..."
+    # python -m pip install isaacsim==4.2.0.2 --extra-index-url https://pypi.nvidia.com
+    # #Install Isaac sim - python package
+    # echo "Downloading Isaac sim - python package ..."
+    # python -m pip install isaacsim-extscache-physics==4.2.0.2 isaacsim-extscache-kit==4.2.0.2 isaacsim-extscache-kit-sdk==4.2.0.2 --extra-index-url https://pypi.nvidia.com
+    # python -m pip install isaacsim-kernel isaacsim-app isaacsim-asset isaacsim-benchmark isaacsim-code-editor isaacsim-core isaacsim-cortex isaacsim-example isaacsim-gui isaacsim-replicator isaacsim-rl isaacsim-robot isaacsim-robot-motion isaacsim-robot-setup isaacsim-ros1 isaacsim-ros2 isaacsim-sensor isaacsim-storage isaacsim-template isaacsim-test isaacsim-utils --extra-index-url https://pypi.nvidia.com
     
     touch ~/.ros/fastdds.xml
     echo '<?xml version="1.0" encoding="UTF-8" ?>
