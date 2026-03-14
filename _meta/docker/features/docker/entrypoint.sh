34109c9877675a4719a4dc5e4eb7980164576e57
Vova Shcherbyna <dev@voshch.dev>
2026-03-15 00:22:48 +0100

    safety first

--- _meta/docker/features/docker/entrypoint.sh
+++ _meta/docker/features/docker/entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+
+set -e
 cd /opt/arena_ws
 source source
 
 if [ ! -f /.built ]; then
     echo "Running initial setup..."
     arena update
-    BUILD_ALL=1 arena build
+    BUILD_ALL=1 arena build || true
     sudo touch /.built
     echo 'Initial setup complete.'
     echo -e 'Run \033[01;33marena feature docker commit\033[0m to cache this state.'
 fi
+
+set +e
 
 exec "$@"
