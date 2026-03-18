feef42e24f613bc32a50732d4ec0a0a6b4245a11
Vova Shcherbyna <dev@voshch.dev>
2026-03-18 17:08:37 +0100

    i love navigation but i would never ever expand launch_navigation.py

--- arena_simulation_setup/launch/nav2.launch.py
+++ arena_simulation_setup/launch/nav2.launch.py
@@ -1,17 +1,21 @@
 import os
 
 from arena_bringup.future import PythonExpression
-from arena_bringup.substitutions import (LaunchArgument, YAMLFileSubstitution,
-                                         YAMLMergeSubstitution,
-                                         YAMLReplaceSubstitution,
-                                         YAMLRetrieveSubstitution)
-from launch import LaunchDescription
-from launch.actions import GroupAction, IncludeLaunchDescription
-from launch.conditions import IfCondition
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from arena_bringup.substitutions import (
+    LaunchArgument,
+    YAMLFileSubstitution,
+    YAMLMergeSubstitution,
+    YAMLReplaceSubstitution,
+    YAMLRetrieveSubstitution,
+)
+from launch.actions import GroupAction
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node, SetRemap
+from launch_ros.descriptions import ParameterFile
 from launch_ros.substitutions import FindPackageShare
+from nav2_common.launch import RewrittenYaml
+
+from launch import LaunchDescription
 
 
 def generate_launch_description():
@@ -35,20 +39,33 @@ def generate_launch_description():
     train_mode = LaunchArgument('train_mode', default_value='false')
 
     substitutions = YAMLMergeSubstitution(
+        # Load default model params
         YAMLFileSubstitution(
             PathJoinSubstitution([
                 ss_root,
                 'configs',
                 'nav2',
+                'defaults',
                 'model_params.yaml'
             ])
         ),
+        # Load robot-specific model params
         YAMLFileSubstitution(
             PathJoinSubstitution([
                 robots_root,
                 'robots',
                 robot.substitution,
                 'model_params.yaml'
+            ])
+        ),
+        # Load default controller params
+        YAMLFileSubstitution(
+            PathJoinSubstitution([
+                ss_root,
+                'configs',
+                'nav2',
+                'defaults',
+                'controller_config.yaml'
             ])
         ),
         # Load controller-specific configuration based on local_planner argument
@@ -62,7 +79,17 @@ def generate_launch_description():
                 'controller_config.yaml'
             ])
         ),
-        # Load controller-specific configuration based on global_planner argument
+        # Load default planner params
+        YAMLFileSubstitution(
+            PathJoinSubstitution([
+                ss_root,
+                'configs',
+                'nav2',
+                'defaults',
+                'planner_config.yaml'
+            ])
+        ),
+        # Load planner-specific configuration based on global_planner argument
         YAMLFileSubstitution(
             PathJoinSubstitution([
                 ss_root,
@@ -73,6 +100,17 @@ def generate_launch_description():
                 'planner_config.yaml'
             ])
         ),
+        # Load default interplanner params
+        YAMLFileSubstitution(
+            PathJoinSubstitution([
+                ss_root,
+                'configs',
+                'nav2',
+                'defaults',
+                'interplanner_config.yaml'
+            ])
+        ),
+        # Load interplanner-specific configuration based on inter_planner argument
         YAMLFileSubstitution(
             PathJoinSubstitution([
                 ss_root,
@@ -150,6 +188,18 @@ def generate_launch_description():
         substitutions=YAMLFileSubstitution(substitutions)
     )
 
+    nav2_configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=substituted_parameters,
+            root_key=namespace.substitution,
+            param_rewrites={
+                'use_sim_time': use_sim_time.substitution,
+            },
+            convert_types=True
+        ),
+        allow_substs=True,
+    )
+
     robot_base_frame = YAMLRetrieveSubstitution(
         YAMLFileSubstitution(substitutions),
         os.path.join('robot_base_frame'),
@@ -184,6 +234,17 @@ def generate_launch_description():
         ('map', PathJoinSubstitution([task_generator_node.substitution, 'map'])),
     ]
 
+    lifecycle_nodes = [
+        'controller_server',
+        'smoother_server',
+        'planner_server',
+        'behavior_server',
+        'bt_navigator',
+        'waypoint_follower',
+        'velocity_smoother',
+        'collision_monitor'
+    ]
+
     bringup_cmd_group = GroupAction([
         *(SetRemap(src=r[0], dst=r[1]) for r in remappings),
         Node(
@@ -199,126 +260,49 @@ def generate_launch_description():
             parameters=[{'odom_frame': 'odom', 'pose_topic': 'pose'}],
             output='screen'
         ),
-        # Node(
-        #     package='tf2_ros',
-        #     executable='static_transform_publisher',
-        #     name='map_broadcaster',
-        #     arguments=['0', '0', '0', '0', '0', '0', 'world', 'map'],
-        #     parameters=[{'use_sim_time': use_sim_time.substitution}]
-        # ),
-        # TF publishers
-        # Node(
-        #     package="tf2_ros",
-        #     executable="static_transform_publisher",
-        #     name="odomframe_to_baseframe_publisher",
-        #     namespace=namespace.substitution,
-        #     arguments=["0", "0", "0", "0", "0", "0", [frame.substitution, robot_odom_frame], [frame.substitution, robot_base_frame]],
-        #     parameters=[use_sim_time.dict],
-        # ),
-        # Node(
-        #     package='robot_localization',
-        #     executable='ekf_node',
-        #     name='ekf_node',
-        #     output='screen',
-        #     parameters=[{
-        #         'use_sim_time': True,
-        #         'odom0': PathJoinSubstitution([task_generator_node.substitution, frame.substitution, robot_odom_frame]),
-        #         'odom0_config': [False, False, False, False, False, False, True, True, False, False, False, True, False, False, False],
-        #         'imu0': PathJoinSubstitution([task_generator_node.substitution, frame.substitution, 'imu/data']),
-        #         'imu0_config': [False, False, False, False, False, False, True, True, True, False, False, True, False, False, False],
-        #         'odom_frame': PathJoinSubstitution([frame.substitution, robot_odom_frame]),
-        #         'base_link_frame': PathJoinSubstitution([frame.substitution, robot_base_frame]),
-        #         'world_frame': PathJoinSubstitution([frame.substitution, robot_odom_frame]),
-        #         'map_frame': 'map',
-        #         'publish_tf': True,
-        #         "two_d_mode": True,
-        #         "frequency": 30.0,
-        #         "transform_time_offset": 0.0,
-        #         "transform_timeout": 0.1,
-        #     }],
-        # ),
-        # AMCL Node
-        # GroupAction([
-        #     Node(
-        #         package='nav2_amcl',
-        #         executable='amcl',
-        #         name='amcl',
-        #         namespace=namespace.substitution,
-        #         output='screen',
-        #         parameters=[{
-        #             'use_sim_time': True,
-        #             'global_frame_id': 'map',
-        #             'map_topic': 'map',
-        #             'odom_frame_id': PathJoinSubstitution([frame.substitution, robot_odom_frame]),
-        #             'base_frame_id': PathJoinSubstitution([frame.substitution, robot_base_frame]),
-        #             'scan_topic': first_observation_source_topic,
-        #             'set_initial_pose': True,
-        #             'always_reset_on_initial_pose': True,
-        #             'initial_pose_x': 0.0,
-        #             'initial_pose_y': 0.0,
-        #             'initial_pose_a': 0.0,
-        #             'odom_model_type': 'diff',
-        #             'alpha1': 0.2,
-        #             'alpha2': 0.2,
-        #             'alpha3': 0.2,
-        #             'alpha4': 0.2,
-        #             'min_particles': 200,
-        #             'max_particles': 2000,
-        #             'pf_err': 0.05,
-        #             'pf_z': 0.99,
-        #             'update_min_d': 0.1,
-        #             'update_min_a': 0.2,
-        #             'laser_max_beams': 640,
-        #             'laser_z_hit': 0.95,
-        #             'laser_z_rand': 0.05,
-        #             'laser_sigma_hit': 0.2,
-        #             'laser_likelihood_max_dist': 2.0,
-        #             'recovery_alpha_slow': 0.001,
-        #             'recovery_alpha_fast': 0.1,
-        #             'transform_tolerance': 0.2,
-        #             'tf_broadcast': True,
-        #         }],
-        #         remappings=[
-        #             ('/map', PathJoinSubstitution([task_generator_node.substitution, 'map'])),
-        #             ('initialpose', PathJoinSubstitution([task_generator_node.substitution, frame.substitution, 'initialpose'])),
-        #         ],
-        #     ),
 
-        #     # Lifecycle Manager for AMCL
-        #     Node(
-        #         package='nav2_lifecycle_manager',
-        #         executable='lifecycle_manager',
-        #         name='lifecycle_manager_navigation',
-        #         output='screen',
-        #         parameters=[
-        #             {'use_sim_time': True},
-        #             {'autostart': True},
-        #             {'node_names': ["amcl"]},
-        #             {'bond_timeout': 0.0},
-        #             {'attempt_respawn_reconnection': True}
-        #         ]
-        #     )],
-        #     condition=IfCondition(PythonExpression(["'", amcl.substitution, "' == 'true'"]))
-        # ),
-        # # Navigation Stack
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                PathJoinSubstitution(
-                    [
-                        pkg_nav2_bringup,
-                        'launch',
-                        'navigation_launch.py'
-                    ]
-                )
-            ),
-            launch_arguments={
-                **use_sim_time.dict,
-                'local_planner': local_planner.substitution,
-                'inter_planner': inter_planner.substitution,
-                'autostart': 'True',
-                'params_file': substituted_parameters,
-                'use_composition': 'False',
-            }.items()
+
+        # nav2 nodes
+        Node(
+            package='nav2_controller', executable='controller_server', name='controller_server',
+            output='screen', parameters=[nav2_configured_params]
+        ),
+        Node(
+            package='nav2_smoother', executable='smoother_server', name='smoother_server',
+            output='screen', parameters=[nav2_configured_params]
+        ),
+        Node(
+            package='nav2_planner', executable='planner_server', name='planner_server',
+            output='screen', parameters=[nav2_configured_params]
+        ),
+        Node(
+            package='nav2_behaviors', executable='behavior_server', name='behavior_server',
+            output='screen', parameters=[nav2_configured_params]
+        ),
+        Node(
+            package='nav2_bt_navigator', executable='bt_navigator', name='bt_navigator',
+            output='screen', parameters=[nav2_configured_params]
+        ),
+        Node(
+            package='nav2_waypoint_follower', executable='waypoint_follower', name='waypoint_follower',
+            output='screen', parameters=[nav2_configured_params]
+        ),
+        Node(
+            package='nav2_velocity_smoother', executable='velocity_smoother', name='velocity_smoother',
+            output='screen', parameters=[nav2_configured_params]
+        ),
+        Node(
+            package='nav2_collision_monitor', executable='collision_monitor', name='collision_monitor',
+            output='screen', parameters=[nav2_configured_params]
+        ),
+        Node(
+            package='nav2_lifecycle_manager', executable='lifecycle_manager', name='lifecycle_manager_navigation',
+            output='screen',
+            parameters=[
+                {'autostart': True},
+                {'node_names': lifecycle_nodes},
+                nav2_configured_params
+            ]
         ),
     ])
 
