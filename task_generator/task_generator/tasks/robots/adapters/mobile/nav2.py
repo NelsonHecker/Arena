c333f50cb6e58db84929e58c3bbcb814007c06b4
Vova Shcherbyna <dev@voshch.dev>
2026-05-17 03:17:35 +0200

    fix nav2 ns
    34c94fd

--- task_generator/task_generator/tasks/robots/adapters/mobile/nav2.py
+++ task_generator/task_generator/tasks/robots/adapters/mobile/nav2.py
@@ -144,7 +144,7 @@ class Nav2Adapter(MobileAdapter):
         robot: RobotManager,
         reset_distance: float = -1.0,
     ) -> bool:
-        node_name = robot.node.service_namespace(robot.name, "local_costmap/local_costmap")
+        node_name = robot.namespace("local_costmap/local_costmap")
 
         if reset_distance < 0:
             srv_name = os.path.abspath(node_name("../clear_entirely_local_costmap"))
